e28c520b0e0c199e392bb0aa00359b03526f3881
Lautaro López <lautaro.lopez98@gmail.com>
2020-11-14 20:31:28 -0300

    Finished update for extras. Added logo on headbar and as favicon, primary color overrided

--- app/controllers/extras_controller.rb
+++ app/controllers/extras_controller.rb
@@ -19,5 +19,25 @@ class ExtrasController < ApplicationController
             render 'new'
           end
     end
+
+    def edit
+        @extra = Extra.find(params[:id])
+    end
+
+    def update
+        @extra = Extra.find(params[:id])
+        if @extra.update_attributes(params.require(:extra).permit(:name, :description, :price))
+          flash[:success] = "Extra was successfully updated"
+          redirect_to root_path
+        else
+            @aux = Extra.find_by name: @extra.name
+            if @aux != nil
+               flash[:error] = "The product already exists"
+            else 
+                flash[:error] = "Something went wrong"
+            end
+            render 'edit'
+        end
+    end
     
 end

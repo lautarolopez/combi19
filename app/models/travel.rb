087248d86e1500ac696cb4303b329746cbaea9ed
Tomás <tomasdozo@hotmail.com>
2020-11-17 18:10:44 -0300

    Atributo y relacion de combi descomentado

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,7 +1,7 @@
 class Travel < ApplicationRecord
 	# Validations
 	validates :driver_id, presence: :true
-	#validates :combi_id, presence: :true
+	validates :combi_id, presence: :true
 	validates :route_id, presence: :true
 	validates :occupied, presence: :true
 	validates :capacity, presence: :true
@@ -14,7 +14,7 @@ class Travel < ApplicationRecord
 	# Relations
 	belongs_to :driver, class_name: 'User', primary_key: driver_id
 	has_many :routes_from, class_name: 'Route', foreign_key: "origin_id", dependent: :destroy
-	#belongs_to :combi
+	belongs_to :combi
 	belongs_to :route
 	has_and_belongs_to_many :passengers, class_name: 'User', dependent: :destroy
 

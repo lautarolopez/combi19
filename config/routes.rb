d7cf696361c9d12b156a5e12ca79a5829bdd8077
Tomás <tomasdozo@hotmail.com>
2020-11-17 10:52:12 -0300

    Revert "Añadir y Modificar Combi"
    This reverts commit bcdbb98899c17ade2f76db1d8f4aa307262976d4.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'combis/new'
-  get 'combis/index'
-  get 'combis/edit'
   devise_for :users, skip: [:sessions, :registrations]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
@@ -20,7 +17,6 @@ Rails.application.routes.draw do
 
   resources :extras
   resources :cities
-  resources :combis
 
   root to: "home#index"
 end

ba13ca9eecdde62c89dc8608bfc8213fa96a48ff
Tomás <tomasdozo@hotmail.com>
2020-11-17 18:17:22 -0300

    Relacion combi

--- app/models/combi.rb
+++ app/models/combi.rb
@@ -1,9 +1,14 @@
 class Combi < ApplicationRecord
+	#Validations
 	validates :licence_plate, presence: true, uniqueness: true,length:{minimum:6,maximun:7}
 	validates :category, presence:true, length:{minimun:0, maximum:50}
 
 	before_save :upcase
 
+	#Relations
+	belongs_to :travel
+
+	#Methods
 	def upcase
 		licence_plate.upcase!
 	end

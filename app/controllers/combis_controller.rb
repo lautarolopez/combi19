99bc0d788629ec02cdd9591f5d5666b829d8c0da
Tomás <tomasdozo@hotmail.com>
2020-11-19 14:33:59 -0300

    feature add modify combi arreglado

--- app/controllers/combis_controller.rb
+++ app/controllers/combis_controller.rb
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 class CombisController < ApplicationController
 	def new
 		if current_user == nil || current_user.role != "admin"
@@ -18,11 +19,38 @@ class CombisController < ApplicationController
 				flash[:error] = "La combi " + @combi.licence_plate + " ya existe."
 			else 
 				flash[:error] = "Algo salió mal."
+=======
+	class CombisController < ApplicationController
+		def new
+			if current_user == nil || current_user.role != "admin"
+				redirect_to root_path
 			end
-			render 'new'
+			@combi = Combi.new
 		end
-	end
 
+		def create
+			@combi = Combi.create(params.require(:combi).permit(:licence_plate, :category));
+
+			if @combi.save
+				flash[:success] = "Combi " + @combi.licence_plate + ", " + @combi.category + " ha sido creada con éxito!"
+				redirect_to combis_path
+			else
+				@aux = Combi.find_by(licence_plate: @combi.licence_plate)
+				if @aux != nil
+					flash[:error] = "La combi " + @combi.licence_plate + " ya existe."
+				else 
+					if @combi.licence_plate == ""
+						flash[:error] = "Falta definir la patente."
+					else
+						flash[:error] = "Algo salió mal."
+					end
+				end
+				render 'new'
+>>>>>>> Stashed changes
+			end
+		end
+
+<<<<<<< Updated upstream
 	def index
 		if current_user == nil || current_user.role != "admin"
 			redirect_to root_path
@@ -51,9 +79,45 @@ class CombisController < ApplicationController
 				flash[:error] = "La combi " + @combi.licence_plate + " ya existe."
 			else 
 				flash[:error] = "Algo salió mal."
+=======
+		def index
+			if current_user == nil || current_user.role != "admin"
+				redirect_to root_path
 			end
-			render 'edit'
+			@combis = Combi.all
 		end
+
+		def edit
+			if current_user == nil || current_user.role != "admin"
+				redirect_to root_path
+			end
+			@combi = Combi.find(params[:id])
+		end
+
+		def update
+			@combi = Combi.find(params[:id])
+			
+			parametros=params.require(:combi).permit(:licence_plate, :category)
+			
+			if @combi.update(parametros)
+				flash[:success] = "Combi " + @combi.licence_plate + ", " + @combi.category + " ha sido actualizada con éxito!"
+				redirect_to combis_path
+			else				
+				if @combi.licence_plate == ""
+					flash[:error] = "Falta definir la patente."
+				else 
+					@aux = Combi.find_by(licence_plate: @combi.licence_plate)
+					if @aux != nil
+						flash[:error] = "La combi " + @combi.licence_plate + " ya existe."
+					else
+						flash[:error] = "Algo salió mal."
+					end
+				end
+				render 'edit'
+>>>>>>> Stashed changes
+			end
+		end
+<<<<<<< Updated upstream
 	end
 
 	def destroy
@@ -72,3 +136,22 @@ class CombisController < ApplicationController
 		redirect_to combis_path
 	end
 end
+=======
+
+		def destroy
+			@combi = Combi.find(params[:id])
+			if (@combi.travels.count > 0)
+				flash[:error] = "Combi " + @combi.licence_plate + ", " + @combi.category + " no se puede borrar porque tiene viajes asociados"
+			else
+				if @combi.destroy
+					flash[:success] = "La combi " + @combi.licence_plate + ", " + @combi.category + " ha sido borrada con éxito!"
+				else
+					if (!flash[:error])
+						flash[:error] = "Algo salió mal"
+					end
+				end
+			end
+			redirect_to combis_path
+		end
+	end
+>>>>>>> Stashed changes

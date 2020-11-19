3a91fe1e0b2c8bc0fa260004c3e8b83f548139ff
Lautaro López <lautaro.lopez98@gmail.com>
2020-11-19 15:16:00 -0300

    fixed combi conflicts, added travel warning

--- app/controllers/combis_controller.rb
+++ app/controllers/combis_controller.rb
@@ -1,25 +1,3 @@
-<<<<<<< Updated upstream
-class CombisController < ApplicationController
-	def new
-		if current_user == nil || current_user.role != "admin"
-			redirect_to root_path
-		end
-		@combi = Combi.new
-	end
-
-	def create
-		@combi = Combi.create(params.require(:combi).permit(:licence_plate, :category));
-
-		if @combi.save
-			flash[:success] = "Combi " + @combi.licence_plate + ", " + @combi.category + " ha sido creada con éxito!"
-			redirect_to combis_path
-		else
-			@aux = Combi.find_by(licence_plate: @combi.licence_plate)
-			if @aux != nil
-				flash[:error] = "La combi " + @combi.licence_plate + " ya existe."
-			else 
-				flash[:error] = "Algo salió mal."
-=======
 	class CombisController < ApplicationController
 		def new
 			if current_user == nil || current_user.role != "admin"
@@ -46,40 +24,9 @@ class CombisController < ApplicationController
 					end
 				end
 				render 'new'
->>>>>>> Stashed changes
 			end
 		end
 
-<<<<<<< Updated upstream
-	def index
-		if current_user == nil || current_user.role != "admin"
-			redirect_to root_path
-		end
-		@combis = Combi.all
-	end
-
-	def edit
-		if current_user == nil || current_user.role != "admin"
-			redirect_to root_path
-		end
-		@combi = Combi.find(params[:id])
-	end
-
-	def update
-		@combi = Combi.find(params[:id])
-		
-		parametros=params.require(:combi).permit(:licence_plate, :category)
-		
-		if @combi.update(parametros)
-			flash[:success] = "Combi " + @combi.licence_plate + ", " + @combi.category + " ha sido actualizada con éxito!"
-			redirect_to combis_path
-        else
-			@aux = Combi.find_by(licence_plate: @combi.licence_plate)
-			if @aux != nil
-				flash[:error] = "La combi " + @combi.licence_plate + " ya existe."
-			else 
-				flash[:error] = "Algo salió mal."
-=======
 		def index
 			if current_user == nil || current_user.role != "admin"
 				redirect_to root_path
@@ -114,29 +61,8 @@ class CombisController < ApplicationController
 					end
 				end
 				render 'edit'
->>>>>>> Stashed changes
 			end
 		end
-<<<<<<< Updated upstream
-	end
-
-	def destroy
-		@combi = Combi.find(params[:id])
-		if (@combi.travels.count > 0)
-			flash[:error] = "Combi " + @combi.licence_plate + ", " + @combi.category + " no se puede borrar porque tiene viajes asociados"
-		else
-			if @combi.destroy
-				flash[:success] = "La combi " + @combi.licence_plate + ", " + @combi.category + " ha sido borrada con éxito!"
-			else
-				if (!flash[:error])
-					flash[:error] = "Algo salió mal"
-				end
-			end
-		end
-		redirect_to combis_path
-	end
-end
-=======
 
 		def destroy
 			@combi = Combi.find(params[:id])
@@ -154,4 +80,3 @@ end
 			redirect_to combis_path
 		end
 	end
->>>>>>> Stashed changes
--- app/models/combi.rb
+++ app/models/combi.rb
@@ -1,20 +1,11 @@
 class Combi < ApplicationRecord
-<<<<<<< Updated upstream
-	# Scopes
-	default_scope -> { order(licence_plate: :asc)}
-
-	#Validations
-=======
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
 	validates :licence_plate, presence: true, uniqueness: true,length:{minimum:6,maximun:7}
-=======
+
 	# Scopes
 	default_scope -> { order(licence_plate: :asc)}
 
 	#Validations
 	validates :licence_plate, presence: true, uniqueness: true
->>>>>>> Stashed changes
 	validates :category, presence:true, length:{minimun:0, maximum:50}
 
 	before_save :upcase

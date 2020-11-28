290e57511f72aea8f75a660b075d9d1787a19a04
Lautaro López <lautaro.lopez98@gmail.com>
2020-11-28 20:55:38 -0300

    created profile, now user is capable of change its data and save it, as well its password

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'combis/new'
   get 'combis/index'
   get 'combis/edit'
-  devise_for :users, skip: [:sessions, :registrations]
+  devise_for :users, skip: [:sessions, :registrations, :passwords]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   as :user do
@@ -11,11 +11,15 @@ Rails.application.routes.draw do
     match 'cerrarsesion', to: 'devise/sessions#destroy', as: :destroy_user_session, via: Devise.mappings[:user].sign_out_via
     get 'cancelarregistro', to: 'devise/registrations#cancel', as: :cancel_user_registration
     get 'registrarse', to: 'devise/registrations#new', as: :new_user_registration
-    get 'editarregistro', to: 'devise/registrations#edit', as: :edit_user_registration
+    get 'perfil', to: 'devise/registrations#edit', as: :edit_user_registration
     patch 'registrarse', to: 'devise/registrations#update', as: :user_registration
     put 'registrarse', to: 'devise/registrations#update'
     delete 'registrarse', to: 'devise/registrations#destroy'
     post 'registrarse', to: 'devise/registrations#create'
+    get 'contraseña/nueva', to: 'devise/passwords#new', as: :new_user_password
+    get 'contraseña/editar', to: 'devise/passwords#edit', as: :edit_user_password
+    put 'contraseña', to: 'devise/passwords#update', as: :user_password
+    post 'contraseña', to: 'devise/passwords#create'
   end
 
   resources :extras

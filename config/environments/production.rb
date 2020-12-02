d56b02f0968a15f9d0b75762035601154990acfa
Lautaro López <lautaro.lopez98@gmail.com>
2020-12-02 14:35:25 -0300

    changed action mailer default url for production, added clean search button, added no travels illustration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.cache_classes = true
 
   #Devise mailer configuration
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'combi19.capendavant.org', protocol: 'http' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
